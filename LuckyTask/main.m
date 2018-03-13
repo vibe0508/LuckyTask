f85fe752db67af65a2001e7071a5691f90caa301
Viacheslav Beltyukov <vyacheslav@eprupetw1237.local>
2018-03-13 10:48:49 +0300

    Initial state

---
+++ LuckyTask/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LuckyTask
+//
+//  Created by Vyacheslav Beltyukov on 3/13/18.
+//  Copyright © 2018 EPAM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
